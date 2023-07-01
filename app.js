c4f45ce0beaca0a00423891133b53de62c44561d
Samrajya Pratap Rana <phinix0980@gmail.com>
2023-07-01 22:47:47 +0545

    project_iniate

---
+++ app.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const route  = require('./routes/routes');
+const connection = require('./models/connection');
+const app = express();
+
+
+
+const PORT = process.env.PORT || 4000
+// middlewares
+app.use(route)
+
+
+// Server
+const server = async () => {
+    await connection(process.env.HOST,process.env.USER,'',process.env.DATABASE)
+    await app.listen(PORT, (err) => {
+        if(err) {
+            return console.log(err.message);
+        }
+        console.log(`Listining on port ${PORT}`);
+    })
+}
+
+
+server();
---
+++ models/connection.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql');
+const connection = (host,user,password,database) => {
+    const con = mysql.createConnection(
+        {
+            host: host,
+            user: user,
+            password: password,
+            database: database
+        }
+    )
+    con.connect((err)=>{
+        if(err){
+            throw err
+        }
+        console.log("Database connected succesfully");
+    })
+};
+
+module.exports = connection;
+
